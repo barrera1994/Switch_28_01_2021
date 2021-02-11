2e2f4bb4051060530c0fe778eb354d1ad5f82430
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-02-11 16:14:55 -0500

    Switch_28_01_2021

---
+++ main.js
@@ -0,0 +1,14 @@
+let numero = parseInt(prompt(`Ingrese un numero entre 1 y 2`));
+switch (numero) {
+     case 1:
+        console.log(`Opcion 1`);
+        break;
+    case 2:
+        console.log(`Opcion 2`);
+        break;
+
+    default:
+        console.log(`Opcion invalida`);
+        break;
+}
+
